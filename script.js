c7611a31ebd5ade5f32a2bf12e7c3e30ebb30c0a
Jarax <85376401+Jarax73@users.noreply.github.com>
2022-05-26 10:01:27 +0000

    incrémentation de points

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//récuperer les éléments html
+
 const accueil = document.querySelectorAll('#accueil .description');
 const form = document.querySelector("#accueil");
 const questionsForm = document.querySelector("#form-questions");
@@ -8,8 +10,12 @@ const questionsNumber = document.querySelector("#questions-number");
 const crossdiv = document.querySelector("#crossdiv");
 const resultScreen = document.querySelector("#result");
 const showScore = document.querySelector("#points");
+let resultName = document.querySelector("#result h1");
+let resultMail = document.querySelector("#result p");
 
-let questions = ["Quel est le type d'un fichier JavaScript ?", "Où est-il conseillé d'écrire du JavaScript ?", "Quelle est l'utilité d'une fonction ?"];
+//Initialiser le contenu de questions
+
+const questions = ["Quel est le type d'un fichier JavaScript ?", "Où est-il conseillé d'écrire du JavaScript ?", "Quelle est l'utilité d'une fonction ?"];
 let answers = [
 	[".ts", ".jsx", ".js", ".j"], 
 	["dans la balise head", "dans la balise body", "dans un fichier externe", "aucune bonne reponse"],
@@ -21,158 +27,152 @@ let idAnswers = [
     ["head", "boDy", "externe", "none"], 
     ["repeter", "balises", "boucle", "same	"]
 ];
-
-
-let indexCurrentQuestion = 0;
+let id = 0;
+let points = 0;
+let index = 0;
 
 resultScreen.style.display = "none";
-
+//L'action à mener à l'écoute de l'évenement "submit"
 form.addEventListener("submit", function(e){
     e.preventDefault();
-
-	const inputValues = {};
-	for (const info of this.elements){
-		const {name, value} = info;
-		if(name){
-		inputValues[name] = value;
-		}
-        
-        // let title = document.querySelector("#result h1");
-        // let email = document.querySelector("result p");
-
-        
-        // title.textContent = info;
-        // email.textContent = info;
-	}
-    for (let ind in inputValues){
-        console.log(ind);
-
-    }
-    console.log(inputValues);
-
+	
+	//récupérer le nom et l'addresse email
+	
+	const name = document.querySelector("#nom").value;
+	const mail = document.querySelector("#mail").value;
+	
+	//fermer l'écran d'accueil et ouvrir l'écran de questions
+	
 	form.style.display = "none";
 	questionsForm.style.display = "block";
-	
-	console.log(questionsForm);
 
+	if(index < questions.length){
+			
+		let oneMinute = 59;
+		display = document.querySelector('#time');
+		startTimer(oneMinute, display);
+		
+		let questionCounter = document.createElement("p");
+		questionCounter.textContent = `Question ${index + 1}/${questions.length}`;
+		console.log(questionCounter);
 
-    if(indexCurrentQuestion < questions.length){
-        let points = 0;
-        let oneMinute = 59;
-        display = document.querySelector('#time');
-        startTimer(oneMinute, display);
+		questionsNumber.prepend(questionCounter);
+
+		let questionTitle = document.createElement("h1");
+		questionTitle.textContent = questions[index];
+		questionsForm.prepend(questionTitle);
+
+		let questionAnswers = document.createElement("div");
+		questionAnswers.setAttribute("id", "radios");
+		let quitbtn = document.createElement("button");
+		quitbtn.setAttribute("id", "orange");
+		quitbtn.textContent  = "Quitter";
+		let nextbtn = document.createElement("button");
+		nextbtn.setAttribute("id", "green");
+		nextbtn.textContent = "Suivant";
+		let stylebtn = document.createElement("div");
+		stylebtn.setAttribute("id", "submit-style");
+
+		
+		questionAnswers.appendChild(stylebtn);
+		const radioIn = document.createElement('div');
+		
+		
+		answers[index].forEach(function(answer){
+			let radioStyle = document.createElement("div");
+			radioStyle.setAttribute("class", "radio-style");
+			let radiosInput = document.createElement("input");
+			radiosInput.setAttribute("type", "radio");
+			radiosInput.setAttribute("name", "answer-choice");
+			let labelTitle = document.createElement("label");
+			labelTitle.classList.add("answer-choice");
+			radiosInput.setAttribute("value", answer);
+			// labelTitle.setAttribute("for", id++);
+			labelTitle.textContent = answer;
+			// radiosInput.setAttribute("id", id++);
+			radioStyle.appendChild(radiosInput);
+			radioStyle.appendChild(labelTitle);
+			radioIn.append(radioStyle);
+			questionAnswers.append(radioIn);
+			console.log(questionAnswers);
+		})
+		
+		// console.log(labelTitle);
+		stylebtn.appendChild(quitbtn);
+		stylebtn.appendChild(nextbtn);
+
+		nextbtn.addEventListener("click", function(e){
+			e.preventDefault();
+			display.remove();
+			
+			startTimer(oneMinute, display);
+			
+			let chose = document.querySelector('input[name="answer-choice"]:checked').value;
+			console.log(chose);
+			
+			
+			
+			
+			
+			if(index < questions.length ){
+				if(chose == correct[index]){
+				points++;
+				console.log(points);
+				}	
+				index++;
 				
-        let questionCounter = document.createElement("p");
-        questionCounter.textContent = `Question ${indexCurrentQuestion + 1}/${questions.length}`;
-        console.log(questionCounter);
-
-        questionsNumber.prepend(questionCounter);
-
-        let questionTitle = document.createElement("h1");
-        questionTitle.textContent = questions[indexCurrentQuestion];
-        questionsForm.prepend(questionTitle);
-
-        let questionAnswers = document.createElement("div");
-        questionAnswers.setAttribute("id", "radios");
-        let quitbtn = document.createElement("button");
-        quitbtn.setAttribute("id", "orange");
-        quitbtn.textContent  = "Quitter";
-        let nextbtn = document.createElement("button");
-        nextbtn.setAttribute("id", "green");
-        nextbtn.textContent = "Suivant";
-        let stylebtn = document.createElement("div");
-        stylebtn.setAttribute("id", "submit-style");
-
-        
-        questionAnswers.appendChild(stylebtn);
-				const radioIn = document.createElement('div');
 				
 				
-        answers[indexCurrentQuestion].forEach(function(answer){
-					let radioStyle = document.createElement("div");
-					radioStyle.setAttribute("class", "radio-style");
-					let radiosInput = document.createElement("input");
-					radiosInput.setAttribute("type", "radio");
-					radiosInput.setAttribute("name", "answer-choice");
-					let labelTitle = document.createElement("label");
-					labelTitle.classList.add("answer-choice");
-					labelTitle.setAttribute("for", answer.split(" "));
-					labelTitle.textContent = answer;
-					radiosInput.setAttribute("id", answer.split(" "));
-					radioStyle.appendChild(radiosInput);
-					radioStyle.appendChild(labelTitle);
-					radioIn.append(radioStyle);
-					questionAnswers.append(radioIn);
-        })
-        stylebtn.appendChild(quitbtn);
-        stylebtn.appendChild(nextbtn);
-
-        nextbtn.addEventListener("click", function(e){
-                e.preventDefault();
-                
-
-					let chose = document.querySelector('input[name="answer-choice"]:checked').value;
-
-					console.log(chose);
-
-					if(indexCurrentQuestion + 1 < questions.length && chose == idAnswers[indexCurrentQuestion ]){
-							points++;
-							indexCurrentQuestion++;
-							
-							
-							questionTitle.textContent = questions[indexCurrentQuestion];
-							questionCounter.textContent = `Question ${indexCurrentQuestion + 1}/${questions.length}`;
-							answers[indexCurrentQuestion].forEach(function(){
-									labelTitle = document.querySelectorAll(".answer-choice");
-									for(let i = 0; i < labelTitle.length; i++){
-											labelTitle[i].textContent = answers[indexCurrentQuestion][i];
-									}
-									
-									questionsForm.reset();
-							})
-					} 
-					else{
-							questionsForm.style.display = "none";
-							resultScreen.style.display = "block";
-							// console.log(indexCurrentQuestion);
-							
-							showScore.textContent = `${points}/${questions.length}`;
+				questionTitle.textContent = questions[index];
+				questionCounter.textContent = `Question ${index + 1}/${questions.length}`;
+				answers.forEach(function(){
+					labelTitle = document.querySelectorAll(".answer-choice");
+					radiosInput = document.querySelectorAll("[type='radio']");
+					for(let i = 0; i < labelTitle.length; i++){
+						labelTitle[i].textContent = answers[index][i];
+						radiosInput[i].setAttribute("value", correct[index]);
 					}
-			});
+				
+					questionsForm.reset();
+				})
+			} 
+			else{
+				points;
+				questionsForm.style.display = "none";
+				resultScreen.style.display = "block";
 
-	questionsForm.appendChild(questionAnswers);
-	console.log(questionsForm);
-	console.log(questionCounter);
-    }
-    
-    else{
-        
-    }
+				resultName = name;
+				resultMail = mail;
+			}
+			showScore.textContent = `${points}/${questions.length}`;
+		});
 
+		questionsForm.appendChild(questionAnswers);
+		console.log(questionsForm);
+		console.log(questionCounter);
+	}
+	
+	else{
+			
+	}
 
-    function startTimer(duration, display) {
-        let timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = duration;
-                if(timer == 0){
+//function timer
+	
+	function startTimer(duration, display) {
+		let timer = duration, minutes, seconds;
+		setInterval(function () {
+				minutes = parseInt(timer / 60, 10);
+				seconds = parseInt(timer % 60, 10);
 
-                }
-            }
+				minutes = minutes < 10 ? "0" + minutes : minutes;
+				seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        }, 1000);
-    }
-    
-   
-        
-  
+				display.textContent = minutes + ":" + seconds;
 
+				if (--timer < 0) {
+						timer = duration;
+				}
+
+		}, 1000);
+	}
 });
