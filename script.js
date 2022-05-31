16b1755ae34cb16ab67b549fe1077c712025f325
Jarax <jaraxkapila73@gmail.com>
2022-05-31 15:40:39 +0100

    review: change on html and js files

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
-//récuperer les éléments html
-const accueil = document.querySelectorAll('#accueil .description');
-const form = document.querySelector("#accueil");
+const form = document.querySelector("#home");
 const questionsForm = document.querySelector("#form-questions");
-const formInput = document.querySelectorAll("#accueil .description");
 const resultScreen = document.querySelector("#result");
 const showScore = document.querySelector("#points");
 const questionTitle = document.createElement("h1");
-questionsForm.append(questionTitle);
 const questionCounter = document.createElement("p");
 const questionsNumber = document.createElement("div");
+const timerQuiz = document.createElement("p");
+const crossdiv = document.createElement('div');
+const crossbar = document.createElement('p');
+const radioIn = document.createElement('div');
+const questionAnswers = document.createElement("div");
+const quitbtn = document.createElement("button");
+const nextbtn = document.createElement("button");
+const stylebtn = document.createElement("div");
+const nameError = document.querySelector("#name");
+const mailError = document.querySelector("#email");
+const resultImage = document.querySelector("img");
+const backWelcome = document.querySelector("#buton");
+const resultName = document.createElement("h1");
+const resultMail = document.createElement("p");
+
+questionsForm.append(questionTitle);
 questionsNumber.append(questionCounter);
 questionsNumber.setAttribute("id", "questions-number");
-const timerQuiz = document.createElement("p");
 timerQuiz.setAttribute("id", "time");
 questionsNumber.append(timerQuiz);
 questionsForm.append(questionsNumber);
-const crossdiv = document.createElement('div');
 crossdiv.setAttribute("id", "crossdiv");
-const crossbar = document.createElement('p');
 crossbar.setAttribute("id", "crossbar");
 crossdiv.appendChild(crossbar);
 questionsForm.appendChild(crossdiv);
-const radioIn = document.createElement('div');
-const questionAnswers = document.createElement("div");
 questionAnswers.setAttribute("id", "radios");
-const quitbtn = document.createElement("button");
 quitbtn.setAttribute("id", "orange");
 quitbtn.textContent  = "Quitter";
-const nextbtn = document.createElement("button");
 nextbtn.setAttribute("id", "green");
 nextbtn.textContent = "Suivant";
-const stylebtn = document.createElement("div");
 stylebtn.setAttribute("id", "submit-style");
-const nameError = document.querySelector("#name");
-const mailError = document.querySelector("#email");
-const success = document.querySelector(".success");
-const wrong = document.querySelector(".wrong");
-const backWelcome = document.querySelector("#buton");
-let resultName = document.createElement("h1");
-let resultMail = document.createElement("p");
 resultScreen.prepend(resultMail);
 resultScreen.prepend(resultName);
-let interval = null;
-let oneMinute = 59;
 
-//Initialiser le contenu de questions
-console.log(form['nom']);
-const questions = ["Quel sera le resultat de 3+2+'7' ?", "Quel est le type d'un fichier JavaScript ?", "Où est-il conseillé d'écrire du JavaScript ?", "Quelle est l'utilité d'une fonction ?", "Comment afficher le message 'Hello World' à l'écran ?", "Comment vider un tableau en JavaScript ?", "Quelle fonction ajoute exactement un élément au début et à la fin d'un tableau ?", "Quelle sera le resultat de ce script : let a = \[1, 2, 3\]; console.log(a\[6\]);",
-"Quel va donner ce code ? let a = console.log(typeof typeof 1);", "Quelle compagnie a développé JavaScript ?",
-"Comment ajouter une valeur à un tableau ?", "Qu'est-ce que ce code renvoie à la console ? : console.log(1 + '2' + '2');", "Dans quel balise HTML plaçons-nous le code JavaScript ?", "Comment faire appelle à une fonction nommée « sum » ?", "Comment écrire une condition IF en JavaScript ?"];
-let answers = [
+const questions = [
+	"Quel sera le resultat de 3+2+'7' ?",
+	"Quel est le type d'un fichier JavaScript ?", 
+	"Où est-il conseillé d'écrire du JavaScript ?", 
+	"Quelle est l'utilité d'une fonction ?", 
+	"Comment afficher le message 'Hello World' à l'écran ?", 
+	"Comment vider un tableau en JavaScript ?", 
+	"Quelle fonction ajoute exactement un élément au début et à la fin d'un tableau ?", 
+	"Quelle sera le resultat de ce script : let a = \[1, 2, 3\]; console.log(a\[6\]);",
+	"Quel va donner ce code ? let a = console.log(typeof typeof 1);", 
+	"Quelle compagnie a développé JavaScript ?",
+	"Comment ajouter une valeur à un tableau ?", 
+	"Qu'est-ce que ce code renvoie à la console ? : console.log(1 + '2' + '2');", 
+	"Dans quel balise HTML plaçons-nous le code JavaScript ?", 
+	"Comment faire appelle à une fonction nommée « sum » ?", 
+	"Comment écrire une condition IF en JavaScript ?"
+];
+const answers = [
 	["327", "12", "14", "57"],
 	[".ts", ".jsx", ".js", ".j"], 
 	["dans la balise head", "dans la balise body", "dans un fichier externe", "aucune bonne reponse"],
@@ -66,12 +73,29 @@ let answers = [
 	["sum()", "call function sum()", "call sum()", "aucune bonne reponse"], 
 	["if a = 2 then", "if a = 2", "if a = 2 else", "if (a == 2)"]
 ];
-let correct = ["14",".js", "dans un fichier externe", "ne pas repéter le même code plusieurs fois", "alert('Hello World');", "arrayList.length=0", "unshift,push", "undefined", "string", "Netscape", "arr[arr.length]=value;", "32", "<script></script>", "sum()", "if (a == 2)"];
+const correct = [
+	"14",
+	".js", 
+	"dans un fichier externe", 
+	"ne pas repéter le même code plusieurs fois", 
+	"alert('Hello World');", 
+	"arrayList.length=0", 
+	"unshift,push", 
+	"undefined", 
+	"string", 
+	"Netscape", 
+	"arr[arr.length]=value;", 
+	"32", 
+	"<script></script>", 
+	"sum()", 
+	"if (a == 2)"
+];
 
+let interval = null;
+let oneMinute = 59;
 let points = 0;
 let index = 0;
 
-resultScreen.style.display = "none";
 
 /**
 *@param {String} name >= 3
@@ -81,7 +105,7 @@ resultScreen.style.display = "none";
 function ValidateEmail(name, mail){
 	if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,3}$/.test(mail) && name.length >= 3)
 	{
-		accueil.style.display = "none";
+		home.style.display = "none";
 		questionsForm.style.display = "block";
 	return (true)
 	}
@@ -102,25 +126,19 @@ function ValidateEmail(name, mail){
 */
 function showResult(name, mail){
 	nextbtn.click();
-	points;
 	showScore.textContent = `${points}/${questions.length}`;
 	resultName.textContent = name;
 	resultMail.textContent = mail;
 	questionsForm.style.display = "none";
 	resultScreen.style.display = "block";
+	resultImage.style.display = "block";
 	if(points >= answers.length / 2){
-		success.style.display = "block";
-		wrong.style.display = "none";
+		resultImage.src = "ant-design_check-circle-outlined.png";
 	}else{
-		wrong.style.display = "block";
-		success.style.display = "none";
+		resultImage.src = "ant-design_close-circle-outlined.png";
 	}
 }
-/**
-*@param {Number} duration
-*@param {Object} display
-*
-*/
+
 function startTimer(duration, display) {
 	clearInterval(interval)
 	let timer = duration, seconds
@@ -139,16 +157,14 @@ function startTimer(duration, display) {
 	}, 1000);
 }
 
-//L'action à mener à l'écoute de l'évenement "submit"
+
 form.addEventListener("submit", function(e){
     e.preventDefault();
 	
-//récupérer le nom et l'addresse email
 	const name = document.querySelector("#nom").value;
 	const mail = document.querySelector("#mail").value;
+
 	if(name != "" && mail != "" && name.length >= 3){
-//action à mener si le nom et le mail sont non-vide et la longueur du nom est >= 3 
-		
 		form.style.display = "none";
 		questionsForm.style.display = "block";
 		if(index < questions.length){
@@ -182,7 +198,8 @@ form.addEventListener("submit", function(e){
 				crossbar.remove();
 				crossdiv.appendChild(crossbar)
 				startTimer(oneMinute, timerQuiz);
-				
+				/*TODO 
+				create a while loop for index < answers.length*/
 				let chose = document.querySelector('input[name="answer-choice"]:checked');
 				if(chose){
 					chose = chose.value;
@@ -191,7 +208,8 @@ form.addEventListener("submit", function(e){
 				if(chose == correct[index]){
 					points++;
 				}
-				index++;				
+				index++;	
+				
 				if(index < answers.length ){
 					questionTitle.textContent = questions[index];
 					questionCounter.textContent = `Question ${index + 1}/${questions.length}`;
@@ -202,7 +220,6 @@ form.addEventListener("submit", function(e){
 							labelTitle[i].textContent = answers[index][i];
 							radiosInput[i].setAttribute("value", answers[index][i]);
 						}
-						questionsForm.reset();
 					})
 				} 
 				else{
